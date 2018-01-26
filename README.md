973df141ab22ff50b9067c7287eb3782e31266ba
Michael Cleary <mcleary03@gmail.com>
2018-01-26 11:46:54 -0800

    refactor with async/await, change return value to a promise, edit readme to reflect this

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # Node Addition
 
 ## Description
-This is a simple node application that will ask you for two numbers and respond with their sum.
-  
+
+This node app will ask you for two numbers and respond with their sum.
+It is indeed overengineered.
+ 
   ### Dependencies
   * [Axios](https://www.npmjs.com/package/axios) for handling POST requests
   * [Inquirer](https://www.npmjs.com/package/inquirer) for handling interactive user input
@@ -12,7 +14,7 @@ This is a simple node application that will ask you for two numbers and respond 
  1. Enter `npm install` in the terminal
  1. Enter `node app.js` to start app
   
-  * *to quit app at any time, press `ctrl + c`*
+    *to quit app at any time, press `ctrl + c`*
 
 
 ## Using the app
@@ -22,10 +24,9 @@ This is a simple node application that will ask you for two numbers and respond 
 
 ```node
 ? Enter the first number
-> 3
+> 2345
 ? Enter the second number
-> 4
-making api call now...
-3 + 4 = 7
+> 64
+Sum of inputs: 2409
 ```
- * The return value is the sum of the two inputs
+ * Returns the sum wrapped in a Promise
--- app.js
+++ app.js
@@ -2,44 +2,43 @@ const axios = require('axios');
 const inquirer = require('inquirer');
 
 const url = 'https://lyft-interview-test.herokuapp.com/test';
+const prompts = [
+  { name: 'x', message: 'Enter the first number\n>' },
+  { name: 'y', message: 'Enter the second number\n>' }
+];
 
-// make axios post request
-const post = (x, y) => {
-  axios.post(url, { x, y })
-    .then(response => {
-      console.log(`${x} + ${y} = ${response.data.sum}`);
-      return response.data.sum;
-    })
-    .catch( error => {
+async function post(x, y) {
+  try {
+    return await axios.post(url, { x, y })
+  } catch(error) {
       console.log(error);
-    });
+  }
 };
 
-const getInput = () => {
+async function getInput() {
+  const args = await inquirer.prompt(prompts);
+  const { x, y } = args;
 
-  inquirer.prompt([
-    { name: 'x', message: 'Enter the first number\n>'},
-    { name: 'y', message: 'Enter the second number\n>'}
-  ])
-  .then( args => {
-    const { x, y } = args;
-
-    if ( validateNumber(x) && validateNumber(y) ) {
-      console.log('making api call now...');
-      return post(x,y);
-    } else {
-      console.log('WARNING: Please only enter numbers');
-      getInput();
-    }
-  });
+  if ( validateNumber(x) && validateNumber(y) ) {
+    return post(x,y);
+  } else {
+    console.log('WARNING: Please only enter numbers');
+    getInput(); // recursively repeat questions until valid input
+  }
 };
 
-// return bool
+// Custom validation because inquirer's built-in function
+//  does not allow me to repeat questions if invalid input
 const validateNumber = n => {
-  return n && !isNaN(n);
+  return n && !isNaN(n); // bool
 };
+
+async function add(promise) {
+  response = await promise();
+  const { sum } = response.data;
+  console.log(`Sum of inputs: ${sum}`);
+  return sum; // this is wrapped in a Promise
+}
 
 // start the app
-getInput();
-
-
+add(getInput);
